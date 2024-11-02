2d0453f60562970e6ef6731e32c1faca2bfcc9fd
Near <mguillermolaura@gmail.com>
2024-11-02 09:53:52 -0500

    creacion del MainApp

---
+++ src/main/java/com/doomTechnologies/MainApp.java
@@ -0,0 +1,28 @@
+package com.doomTechnologies;
+
+//import com.doomTechnologies.view.Login;
+import com.formdev.flatlaf.FlatLightLaf;
+
+import javax.swing.*;
+
+public class MainApp {
+    public static void main(String[] args) {
+
+//        FlatLightLaf.setup();
+//        initStyle();
+//        Login lg = new Login();
+//        lg.setLocationRelativeTo(null);
+//        lg.setVisible(true);
+    }
+//    public static void initStyle(){
+//        UIManager.put( "Button.arc", 999 );
+//        UIManager.put( "Component.arc", 999 );
+//        UIManager.put( "ProgressBar.arc", 999 );
+//        UIManager.put( "TextComponent.arc", 999 );
+//        UIManager.put( "[style]Panel.myRoundPanel",
+//                "[light]background: tint(@background,50%);" +
+//                        "[dark]background: shade(@background,15%);" +
+//                        "[light]border: 16,16,16,16,shade(@background,10%),,20;" +
+//                        "[dark]border: 16,16,16,16,tint(@background,10%),,20" );
+//    }
+}
